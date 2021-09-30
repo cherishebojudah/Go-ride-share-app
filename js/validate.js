ba9ddc382e06172a4de520763803021f34e6dcd9
Cherish Collins <cherish.ebojudah@gmail.com>
2021-09-30 11:56:11 +0100

    creating form validation with js

---
+++ js/validate.js
@@ -0,0 +1,13 @@
+//target the form an all the form elements
+let submitForm = document.querySelector('#validateForm'),
+    fullName = document.querySelector('#fullName'),
+    email = document.querySelector('#email'),
+    phoneNumber = document.querySelector('#phoneNumber'),
+    gender = document.querySelector('#gender'),
+    password = document.querySelector('#password');
+
+
+    //this function makes sure all required fields are filled
+    function required(field, event) {
+        
+    }
