efa81b7b0c929a3b9078b09c729b7040241e61dc
Cherish Collins <cherish.ebojudah@gmail.com>
2021-09-30 12:06:00 +0100

    all required fields validation

--- js/validate.js
+++ js/validate.js
@@ -9,5 +9,31 @@ let submitForm = document.querySelector('#validateForm'),
 
     //this function makes sure all required fields are filled
     function required(field, event) {
-        
+        if (field.value == '') {
+            field.nextElementSibling.innerHTML = 'This is a required field!';
+            event.preventDefault();
+            return false;
+        } else {
+            field.nextElementSibling.innerHTML = '';
+            return true;
+        }
     }
+
+    // add eventlistener
+    function validateForm(e) {
+        required(fullName, e);
+        required(email, e);
+        required(phoneNumber, e);
+        required(gender, e);
+        required(password, e);
+
+        return true;
+    }
+
+
+    //email validation
+    function emailValidation(event) {
+        let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    }
+
+    submitForm.addEventListener('submit', validateForm);
