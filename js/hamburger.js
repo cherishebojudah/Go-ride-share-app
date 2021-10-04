b03a7d94e5a34981225a41ad0033f522ab6591dc
Cherish Collins <cherish.ebojudah@gmail.com>
2021-10-04 18:23:22 +0100

    debugged js to fix hamburger menu

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,8 +1,8 @@
 function myFunction() {
-  var menu = document.getElementById("myLinks");
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
-  }
+    var menu = document.getElementById("nav-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
 }
